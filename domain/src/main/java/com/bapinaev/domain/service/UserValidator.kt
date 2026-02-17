72c3543c14a638230a9cd6e8b105294272fe2c06
Vladislav Ayrumov <vladislavayrumov@gmail.com>
2026-02-18 00:50:21 +0300

    feat: used when instead if

--- domain/src/main/java/com/bapinaev/domain/service/UserValidator.kt
+++ domain/src/main/java/com/bapinaev/domain/service/UserValidator.kt
@@ -6,15 +6,20 @@ import com.bapinaev.domain.model.User
 class UserValidator {
 
     fun execute(user: User) {
-        if (user.login.isEmpty())
-            throw InvalidUserInfoException("User login must not be blank")
-        if (user.firstName.isBlank())
-            throw InvalidUserInfoException("User firstName must not be blank")
-        if (user.surname.isBlank())
-            throw InvalidUserInfoException("User surname must not be blank")
-        if (user.password.isBlank() || user.password.length < 5)
-            throw InvalidUserInfoException(
-                "User password must not be blank and be more than 5 characters"
-            )
+        when {
+            user.login.isBlank() -> throw InvalidUserInfoException("User login must not be blank")
+
+            user.firstName.isBlank() ->
+                throw InvalidUserInfoException("User firstName must not be blank")
+
+            user.surname.isBlank() ->
+                throw InvalidUserInfoException("User surname must not be blank")
+
+            user.password.isBlank() || user.password.length < 5 ->
+                throw InvalidUserInfoException(
+                    "User password must not be blank and be more than 4 characters"
+                )
+        }
     }
+
 }
